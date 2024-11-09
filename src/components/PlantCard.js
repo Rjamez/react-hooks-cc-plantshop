1442e44303fed037c0876f6bc9bbdac47ecf5465
Rjamez <robin.adhola.student@moringaschool.com>
2024-11-09 10:04:57 +0300

    src

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,17 @@
-import React from "react";
+// PlantCard.js
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const [isInStock, setIsInStock] = useState(true);
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={process.env.PUBLIC_URL + plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price}</p>
+      <button onClick={() => setIsInStock(!isInStock)} className={isInStock ? "primary" : ""}>
+        {isInStock ? "In Stock" : "Out of Stock"}
+      </button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,27 @@
-import React from "react";
+// PlantPage.js
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("https://phase2-week2-plantsy-code-challenge.onrender.com/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm onAddPlant={handleAddPlant} />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
